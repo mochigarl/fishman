305eee171d9a72c02cf18c8794ab7eeadfb48df9
shiffaazaharrr <shfazhr03@gmail.com>
2026-06-06 22:44:40 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Dashboard from "./pages/Dashboard"
 import Orders from "./pages/Orders"
